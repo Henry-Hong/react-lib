9ccd608877167285d45fad0e955005820e7a412c
Henry-Hong <hhr8540@naver.com>
2024-05-31 14:27:02 +0900

    feat : useEffecOnce 추가

---
+++ src/hooks/useEffectOnce.ts
@@ -0,0 +1,35 @@
+import { EffectCallback, useEffect, useRef } from "react";
+
+/**
+ * ### Overview
+ * @hooks useEffectOnce
+ * @param {function} callback - 한번만 실행할 콜백함수
+ * @returns void 0
+ * ---
+ * ### Details
+ * @description
+ * - 1. 컴포넌트 라이프 사이클에서 단 한번만 실행합니다.
+ * - 2. React.StrictMode에서도 단 한번만 실행됩니다.
+ * @example
+ * ```
+ * // EXAMPLE_START
+ * useEffectOnce(() => console.log("log only once"));
+ * // EXAMPLE_END
+ * ```
+ */
+export const useEffectOnce = (callback: EffectCallback) => {
+  const isMounted = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      callback();
+    }
+    return () => {
+      if (typeof callback === "function") {
+        callback();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
